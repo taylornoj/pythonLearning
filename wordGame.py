cebb295711b60c39d9362e16d106736d9705b97f
Taylor Noj <taylornoj@gmail.com>
2022-03-21 16:16:26 -0400

    input working and print input back after prose

--- wordGame.py
+++ wordGame.py
@@ -1,12 +1,12 @@
-"""
-Planning:
-- ask user for a word (get input)
-- replace section of story with users word in backend
-- repeat until all blanks are filled
-- print out final string
-"""
+# """
+# Planning:
+# - ask user for a word (get input)
+# - replace section of story with users word in backend
+# - repeat until all blanks are filled
+# - print out final string
+# """
 
-string = """
+proseString = """
 Hi mom,
 
 Just writing to tell you that I've quit my job as a OCCUPATION and I'm moving to COUNTRY. The truth is, I've always been passionate about PLURAL_NOUN, and COUNTRY is the best place in the world to build a career around them. I'll need to start small-- At first, all I'll be allowed to do is to VERB near them, but when people see how ADJECTIVE I can be, I'm sure to rise to the top.
@@ -17,4 +17,14 @@ Love,
 
 NAME
 """
-print(string)
+# print(proseString)
+
+userInput = input("Please provide a holiday: ")
+
+print(proseString)
+print(userInput)
+
+# newProseString = proseString.replace("HOLIDAY", userInput)
+# proseString = newProseString
+
+# print(newProseString)
