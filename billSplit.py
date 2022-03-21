056909be545a9c020b6786205d72833eed1894c4
Taylor Noj <taylornoj@gmail.com>
2022-03-21 12:25:54 -0400

    add notes for methods

--- billSplit.py
+++ billSplit.py
@@ -1,4 +1,6 @@
+# float() method returns floating point number from a number or a string
 billTotal = float(input("Bill total: "))
+# int() method returns an integer object from any number or string
 tipPercent = int(input("Tip percentage: "))
 numOfPeople = int(input("Number of people: "))
 
@@ -11,5 +13,6 @@ total = round(billTotal + tax + tipPercent, 2)
 
 totalPerPerson = round(total / numOfPeople, 2)
 
+# str() used to convert specified value into a string
 print("The total is: $ " + str(total))
 print("Each person should pay: $" + str(totalPerPerson))
