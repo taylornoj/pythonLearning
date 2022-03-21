262979c5a58344befad76d00402ca27aa0851c6e
Taylor Noj <taylornoj@gmail.com>
2022-03-21 15:20:33 -0400

    add prose for game

--- wordGame.py
+++ wordGame.py
@@ -5,3 +5,16 @@ Planning:
 - repeat until all blanks are filled
 - print out final string
 """
+
+string = """
+Hi mom,
+
+Just writing to tell you that I've quit my job as a OCCUPATION and I'm moving to COUNTRY. The truth is, I've always been passionate about PLURAL_NOUN, and COUNTRY is the best place in the world to build a career around them. I'll need to start small-- At first, all I'll be allowed to do is to VERB near them, but when people see how ADJECTIVE I can be, I'm sure to rise to the top.
+
+Don't worry about me, and tell dad to take good care of my PERSONAL_ITEM. I'll be sure to call every HOLIDAY.
+
+Love,
+
+NAME
+"""
+print(string)
