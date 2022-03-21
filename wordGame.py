0ff4299790fc36270258012e7456ab54b719eb8d
Taylor Noj <taylornoj@gmail.com>
2022-03-21 16:42:09 -0400

    add multiple user inputs to complete whole mad lib

--- wordGame.py
+++ wordGame.py
@@ -9,9 +9,14 @@
 proseString = """
 Hi mom,
 
-Just writing to tell you that I've quit my job as a OCCUPATION and I'm moving to COUNTRY. The truth is, I've always been passionate about PLURAL_NOUN, and COUNTRY is the best place in the world to build a career around them. I'll need to start small-- At first, all I'll be allowed to do is to VERB near them, but when people see how ADJECTIVE I can be, I'm sure to rise to the top.
+Just writing to tell you that I've quit my job as a OCCUPATION and I'm moving to 
+COUNTRY. The truth is, I've always been passionate about PLURAL_NOUN, and COUNTRY 
+is the best place in the world to build a career around them. I'll need to start 
+small-- At first, all I'll be allowed to do is to VERB near them, but when people 
+see how ADJECTIVE I can be, I'm sure to rise to the top.
 
-Don't worry about me, and tell dad to take good care of my PERSONAL_ITEM. I'll be sure to call every HOLIDAY.
+Don't worry about me, and tell dad to take good care of my PERSONAL_ITEM. I'll be 
+sure to call every HOLIDAY.
 
 Love,
 
@@ -22,8 +27,24 @@ newProseString = proseString
 userInput = input("Please provide an occupation: ")
 newProseString = newProseString.replace("OCCUPATION", userInput)
 
+userInput = input("Please provide country: ")
+newProseString = newProseString.replace("COUNTRY", userInput)
+
+userInput = input("Please provide a plural noun: ")
+newProseString = newProseString.replace("PLURAL_NOUN", userInput)
+
+userInput = input("Please provide a verb: ")
+newProseString = newProseString.replace("VERB", userInput)
+
+userInput = input("Please provide an adjective: ")
+newProseString = newProseString.replace("ADJECTIVE", userInput)
+
+userInput = input("Please provide a personal item: ")
+newProseString = newProseString.replace("PERSONAL_ITEM", userInput)
+
 userInput = input("Please provide a holiday: ")
 newProseString = newProseString.replace("HOLIDAY", userInput)
+
 
 
 print(newProseString)
