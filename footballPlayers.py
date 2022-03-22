4ced0554731128c77d3751fc615f7df544696660
Taylor Noj <taylornoj@gmail.com>
2022-03-22 15:15:10 -0400

    add list of footballers

---
+++ footballPlayers.py
@@ -0,0 +1,22 @@
+footballers = [
+  "Mia",
+  "Retta",
+  "Augustine",
+  "Jin",
+  "Waylon",
+  "Alphonso",
+  "Sage",
+  "Hubert",
+  "Raymon",
+  "Rebecca",
+  "Monty",
+  "Glen",
+  "Christi",
+  "Patrice",
+  "Craig",
+  "Dexter",
+  "Wally",
+  "Ian",
+  "Pat"
+]
+
