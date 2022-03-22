6626ce85ff5731bafd2e1ca92dc0a40746db30aa
Taylor Noj <taylornoj@gmail.com>
2022-03-22 12:38:47 -0400

    merge todo lists

--- changeList.py
+++ changeList.py
@@ -13,4 +13,23 @@ print(todo)
 todo.append("Wash car")
 print(todo) 
 
-test
+# places "Clean kitchen" in index 1
+todo.insert(1, "Clean kitchen")
+print(todo)
+
+# changes "Buy groceries" to "Unload groceries"
+todo[0] = "Unload groceries"
+print(todo)
+
+# make a second list and...
+tomorrowsTodo = [
+  "Walk dog",
+  "Clean garden",
+  "Call friend",
+  "Put away laundry"
+]
+
+# add it to original list to print everything together
+todo = todo + tomorrowsTodo
+
+print(todo)
