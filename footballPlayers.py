250e24b5eb140d842e52fe0e78371c2fce1cc230
Taylor Noj <taylornoj@gmail.com>
2022-03-22 15:25:15 -0400

    get two before and after median using : list slicing

--- footballPlayers.py
+++ footballPlayers.py
@@ -27,3 +27,6 @@ print(len(footballers))
 # find median
 median = len(footballers) // 2
 print(footballers[median])
+
+# 2. get 5 players in middle of league ---
+print(footballers[median-2:median+3])
