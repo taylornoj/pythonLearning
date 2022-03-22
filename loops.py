81aacddd6ecf85a8f4ad6df4bdd56d55c224dda5
Taylor Noj <taylornoj@gmail.com>
2022-03-22 19:48:57 -0400

    add example of for loop with correct indentation

--- loops.py
+++ loops.py
@@ -5,3 +5,18 @@
 
 # The : denotes that the next lines are part of a block# 
 # and the indentation on line 4 is important
+
+# instead of using { } like in JS, Python uses indentations
+
+clothing = [
+  "Shirt",
+  "Pants",
+  "Socks"
+]
+
+for item in clothing:
+  foldedItem = "Folded " + item
+  print(foldedItem)
+print("This line is not part of the `for` loop")
+
+# line 20 will only run once  lines 8 and 9 have gone through every items in 'clothing'
