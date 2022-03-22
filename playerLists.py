594ef583b465817841a059e4cb6fc942cf171435
Taylor Noj <taylornoj@gmail.com>
2022-03-22 09:46:44 -0400

    add list of players

---
+++ playerLists.py
@@ -0,0 +1,18 @@
+players = [
+  "Mia",
+  "Retta",
+  "Waylon",
+  "Jamel",
+  "Sage",
+  "Hubert",
+  "Monty",
+  "Glen",
+  "Christie",
+  "Patrice",
+  "Craig",
+  "Ian",
+  "Sam",
+  "Wally",
+  "Pat"
+]
+
