a23c52bf166b8e4e483a33141045d2f6fd6a3eca
Taylor Noj <taylornoj@gmail.com>
2022-03-23 14:09:52 -0400

    import time method

--- iterateOverRange.py
+++ iterateOverRange.py
@@ -1,9 +1,13 @@
 # keep doubling the bacteria population for n generations
 # n is the difference between the first and last number in the range
 
+# import Pythons build in time library to put pause between each generation
+import time
+
 bacteria = "🌭"
 
 for generation in range(0, 10):
   bacteria = bacteria + bacteria
 
 print(bacteria)
+
