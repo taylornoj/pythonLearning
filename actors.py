222675ef9a5eca0059432deca51608df44c74b1e
Taylor Noj <taylornoj@gmail.com>
2022-03-23 15:19:33 -0400

    prep for next Actors exercise

---
+++ actors.py
@@ -0,0 +1,23 @@
+actors = [
+  "Nathan Fillion",
+  "Gina Torres",
+  "Alan Tudyk",
+  "Morena Baccarin",
+  "Adam Baldwin",
+  "Jewel Staite",
+  "Sean Maher",
+  "Summer Glau",
+  "Ron Glass"
+  ]
+
+roles = [
+  "Captain Malcolm Reynolds",
+  "Zoe Washburn",
+  "Hoban Washburn",
+  "Inara Serra",
+  "Jayne Cobb",
+  "Kaylee Frye",
+  "Dr. Simon Tam",
+  "River Tam",
+  "Derrial Book"
+]
