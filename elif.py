9c161a519c8392a0cd90162605f62e2d2fce66dc
Taylor Noj <taylornoj@gmail.com>
2022-03-26 17:17:46 -0400

    elif example

---
+++ elif.py
@@ -0,0 +1,9 @@
+age = 16
+
+print("Can you ride this rollercoaster?")
+if age > 18:
+  print("Definitely!")
+elif age > 12:
+  print("Maybe, how tall are you?")
+else:
+  print("Sorry, kid. Come back when you're older")
