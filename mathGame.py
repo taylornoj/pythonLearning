2c7a197b03929c021c9cd5526a7b5794206668cb
Taylor Noj <taylornoj@gmail.com>
2022-04-01 22:31:29 -0400

    add counter for score on correct answer

--- mathGame.py
+++ mathGame.py
@@ -11,5 +11,6 @@ for a, b in questions:
   response = int(input("What is the value of " + str(a) + " + " + str(b) + "? "))
   if response == a + b:
     print("Correct!")
+    score += 1
   else:
     print("Wrongo!")
