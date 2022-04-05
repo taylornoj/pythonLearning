f14eae3a67322bf8901dd1e7d5b97e52aba450f2
Taylor Noj <taylornoj@gmail.com>
2022-04-05 11:51:43 -0400

    options for coice 1 and two working in loop

--- tempConverter.py
+++ tempConverter.py
@@ -2,3 +2,10 @@ print("Choose from the options below:")
 print("1. Celsius to Fahrenheit")
 print("2. Fahrenheit to Celsius")
 option = int(input("Option 1 or 2: "))
+
+if option == 1:
+  celsius = float(input("Temperature in Celsius: "))
+elif option == 2:
+  fahrenheit = float(input("Temperature in Fahrenheit: "))
+else:
+  print("Choose option 1 or 2")
