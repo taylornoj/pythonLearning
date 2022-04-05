68d529563ac639527b062c82a48479b5a081466f
Taylor Noj <taylornoj@gmail.com>
2022-04-05 09:56:01 -0400

    add time method

--- diceRoll.py
+++ diceRoll.py
@@ -1,8 +1,11 @@
 from random import randint
+import time
 
 repeat = True
 
 while repeat:
+  print("Rolling dice...")
+  time.sleep(2)
   print("You rolled ", randint(1,6), " and ", randint(1,6))
   print("Do you want to roll again? Y/N")
   repeat = "Y" in input()
