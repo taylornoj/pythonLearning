d05f986d9a4b40f453fe724ab0e6b42c5998b99c
Taylor Noj <taylornoj@gmail.com>
2022-04-05 09:44:00 -0400

    better loop

--- diceRoll.py
+++ diceRoll.py
@@ -1,10 +1,6 @@
-import random
+from random import randint
 
-min_val = 1
-max_val = 6
+repeat = True
 
-roll_again = "yes"
-
-while roll_again == "yes" or roll_again == "y":
-  print("Rolling the Dices...")
-  print("The values are: ")
+while repeat:
+  print("You rolled ", randint(1,6), " and ", randint(1,6))
