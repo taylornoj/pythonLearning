16abc725a1369d9aeb148a5c674a8f101012b254
Taylor Noj <taylornoj@gmail.com>
2022-04-15 17:30:11 -0400

    add window seat challenge

---
+++ windowSeat.py
@@ -0,0 +1,14 @@
+# e = empty seat
+# o = occupied seat
+# capital letters indicate a window seat
+# Dot needs to be in an empty window seat with an empty seat beside them
+
+layout = [
+    ["O","e","e",None, "e","o","e",None, "o","o","E"], # row 1
+    ["O","o","e",None, "e","o","e",None, "e","o","E"], # row 2
+    ["O","o","o",None, "o","o","e",None, "o","e","O"], # row 3
+    ["E","o","e",None, "e","o","e",None, "o","e","E"], # row 4 
+    ["E","e","o",None, "e","e","e",None, "o","o","E"], # row 5
+    ["O","e","e",None, "e","e","e",None, "e","e","E"]  # row 6
+]
+
