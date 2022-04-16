65be48230a52cd13dc5da6e8397d7c90f4739039
Taylor Noj <taylornoj@gmail.com>
2022-04-15 21:32:58 -0400

    add dictionary for landmarks

---
+++ waitTime.py
@@ -0,0 +1,15 @@
+
+
+landmarks = {
+    "Big Ben": 12,
+    "Tower Bridge": 25,
+    "Buckingham Palace": 15,
+    "Madame Tussauds": 25,
+    "London Eye": 40,
+    "Tower of London": 25,
+    "Emirates Air Line cable car": 16,
+    "London Transport Museum": 7,
+    "Wembley Stadium": 8,
+    "Hyde Park": 0,
+    "The View from The Shard": 14
+}
